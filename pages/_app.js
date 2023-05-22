ca734f9e4e7bbb37b688291f06ff96f2f1c98070
ngovanvien1010 <ngovanvien1010@gmail.com>
2023-05-23 00:40:51 +0700

    create donate page

--- pages/_app.js
+++ pages/_app.js
@@ -12,20 +12,36 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const [supabase] = useState(() =>
+    createBrowserSupabaseClient({
+      supabaseUrl: 'https://xlafgkujgoaelkhxzzpo.supabase.co',
+      supabaseKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsYWZna3VqZ29hZWxraHh6enBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3Mzc2NzAsImV4cCI6MjAwMDMxMzY3MH0.TDJjcR6Hi6a38uZ8EBBGtEja8mJ6_Yh0Q2u3OvXku-I',
+    })
+  )
+
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        {isDevelopment && isSocket && <ClientReload />}
+        <Analytics />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </SessionContextProvider>
     </ThemeProvider>
   )
 }
---
+++ pages/donate.js
@@ -0,0 +1,120 @@
+import siteMetadata from '@/data/siteMetadata'
+import projectsData from '@/data/projectsData'
+import Card from '@/components/Card'
+import { PageSEO } from '@/components/SEO'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useCallback, useEffect, useState } from 'react'
+import Link from '@/components/Link'
+import Tag from '@/components/Tag'
+import Pagination from '@/components/Pagination'
+
+const POSTS_PER_PAGE = 10
+
+export default function Donate() {
+  const [donate, setDonate] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+  const supabase = useSupabaseClient()
+
+  const getProfile = useCallback(async () => {
+    try {
+      setLoading(true)
+
+      let { data, error, status } = await supabase
+        .from('donate')
+        .select(`id, title, description, image, link`)
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setDonate(data)
+      }
+    } catch (error) {
+      alert('Error loading user data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }, [supabase])
+
+  useEffect(() => {
+    getProfile()
+  }, [getProfile])
+
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.ceil(donate.length / POSTS_PER_PAGE),
+  }
+
+  return (
+    <>
+      <PageSEO title={`Blog - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <>
+        <div className="divide-y divide-gray-200 dark:divide-gray-700">
+          <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              Donate
+            </h1>
+            <div className="relative max-w-lg">
+              <input
+                aria-label="Search articles"
+                type="text"
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder="Search articles"
+                className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+              />
+              <svg
+                className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+          </div>
+          <ul>
+            {!donate.length && 'No donate found.'}
+            {donate.map(({ id, title, link, image, description }) => {
+              return (
+                <li key={id} className="py-4">
+                  <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      {/* <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd> */}
+                    </dl>
+                    <div className="space-y-3 xl:col-span-3">
+                      <div>
+                        <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                          <a href={link} className="text-gray-900 dark:text-gray-100">
+                            {title}
+                          </a>
+                        </h3>
+                      </div>
+                      <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        {description}
+                      </div>
+                    </div>
+                  </article>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        {pagination && pagination.totalPages > 1 && !searchValue && (
+          <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
+        )}
+      </>
+    </>
+  )
+}
