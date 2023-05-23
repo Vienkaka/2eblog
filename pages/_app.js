e6cd09b37629df086f0691be28149ca7ae461af1
ngovanvien1010 <ngovanvien1010@gmail.com>
2023-05-23 16:19:22 +0700

    use env

--- pages/_app.js
+++ pages/_app.js
@@ -22,13 +22,7 @@ const isSocket = process.env.SOCKET
 export default function App({ Component, pageProps }) {
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const [supabase] = useState(() =>
-    createBrowserSupabaseClient({
-      supabaseUrl: 'https://xlafgkujgoaelkhxzzpo.supabase.co',
-      supabaseKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsYWZna3VqZ29hZWxraHh6enBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3Mzc2NzAsImV4cCI6MjAwMDMxMzY3MH0.TDJjcR6Hi6a38uZ8EBBGtEja8mJ6_Yh0Q2u3OvXku-I',
-    })
-  )
+  const [supabase] = useState(() => createBrowserSupabaseClient({ supabaseUrl, supabaseKey }))
 
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
--- pages/donate.js
+++ pages/donate.js
@@ -7,6 +7,7 @@ import { useCallback, useEffect, useState } from 'react'
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import Pagination from '@/components/Pagination'
+import formatDate from '@/lib/utils/formatDate'
 
 const POSTS_PER_PAGE = 10
 
@@ -59,10 +60,10 @@ export default function Donate() {
             </h1>
             <div className="relative max-w-lg">
               <input
-                aria-label="Search articles"
+                aria-label="Tìm sản phẩm"
                 type="text"
                 onChange={(e) => setSearchValue(e.target.value)}
-                placeholder="Search articles"
+                placeholder="Tìm sản phẩm"
                 className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
               />
               <svg
@@ -86,14 +87,11 @@ export default function Donate() {
             {donate.map(({ id, title, link, image, description }) => {
               return (
                 <li key={id} className="py-4">
-                  <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      {/* <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date)}</time>
-                      </dd> */}
-                    </dl>
-                    <div className="space-y-3 xl:col-span-3">
+                  <article className="space-y-2 xl:grid xl:grid-cols-4">
+                    <div className="bg-sky-500/100">
+                      <p>hello</p>
+                    </div>
+                    <div className="ml-1 space-y-3 xl:col-span-3">
                       <div>
                         <h3 className="text-2xl font-bold leading-8 tracking-tight">
                           <a href={link} className="text-gray-900 dark:text-gray-100">
